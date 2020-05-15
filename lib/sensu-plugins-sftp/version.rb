ee54d2bc19cd5d021d6c493c9bede06b96e92cec
Todd Campbell <todd@sensu.io>
2020-05-15 11:29:00 -0700

    Rolled version
    Signed-off-by: Todd Campbell <todd@sensu.io>

--- lib/sensu-plugins-sftp/version.rb
+++ lib/sensu-plugins-sftp/version.rb
@@ -3,9 +3,9 @@
 module SensuPluginsSftp
   # This defines the version of the gem
   module Version
-    MAJOR = 1
+    MAJOR = 2
     MINOR = 0
-    PATCH = 1
+    PATCH = 0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
